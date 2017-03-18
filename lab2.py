69890fdef4720da70836ceee588585ccead24dbf
AndreyKuzmin <andrey-58rus@yandex.ru>
2017-03-18 11:15:42 +0300

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,14 @@
+def ger_sqrt(num):
+    x = 1
+    sq = False
+    while sq == False:
+        pre = x
+        x = 0.5 * (x + num / x)
+        if  x ** 2 == num or x == pre:
+            sq = True
+    return x;
+
+print('введите число')
+num = int(input())
+x = ger_sqrt(num)
+print(x)
